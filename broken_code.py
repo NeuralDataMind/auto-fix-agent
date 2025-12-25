33645ee6bbf23fd95ab7a4cda85fffb2c0dbfe8d
Mallikarjun Reddy Bardipuram <sunnygalaxym12@gmail.com>
2025-12-25 22:24:06 +0530

    fix broken_code.py

--- broken_code.py
+++ broken_code.py
@@ -2,7 +2,6 @@ def add_numbers(a, b):
     try:
         return a + int(b)
     except ValueError:
-<<<<<<< HEAD
         try:
             return float(a) + float(b)
         except ValueError:
@@ -10,11 +9,6 @@ def add_numbers(a, b):
                 return complex(a) + complex(b)
             except ValueError:
                 raise TypeError("Unsupported operand type(s) for +")
-=======
-        return "Error: unable to convert 'b' to integer"
-    except TypeError:
-        return "Error: unsupported operand type(s) for +"
->>>>>>> ee2e8c19f1b53176a1fe8fddae528faf697d2548
 
 def get_first_element(my_list):
     if len(my_list) > 0:
