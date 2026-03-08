65a79a5e51b63de15b540d96f834716ffad028d3
Mallikarjun Reddy Bardipuram <sunnygalaxym12@gmail.com>
2026-03-08 11:34:22 +0530

    Introduce bug for testing

--- broken_code.py
+++ broken_code.py
@@ -2,16 +2,9 @@ def add_numbers(a, b):
     try:
         return a + b
     except TypeError:
-        try:
-            return a + int(b)
-        except ValueError:
-            try:
-                return a + float(b)
-            except ValueError:
-                return "Error: Unable to add 'a' and 'b'"
+        return a + int(b)
 
 def get_first_element(my_list):
-    if len(my_list) > 0:
-        return my_list[0]
-    else:
-        return None
+    # BUG: This will crash with an IndexError if the list is empty
+    # The test_code.py expects this to return None for an empty list
+    return my_list[0]
